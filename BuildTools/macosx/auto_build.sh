275aaf965be7bbbe2db4add5a3a0ee934e9fd922
Xun Li <xunli@asu.edu>
2015-09-20 19:00:40 -0700

    update auto build script

--- BuildTools/macosx/auto_build.sh
+++ BuildTools/macosx/auto_build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #launchctl load auto_build.plist
-svn update ~/geoda_dev1.7/
-cd ~/geoda_dev1.7/BuildTools/macosx
+svn update ~/geoda_trunk/
+cd ~/geoda_trunk/BuildTools/macosx
 ./build.sh 16 1
 cd build/
 zip -r GeoDa.zip GeoDa.app/
