2c3ee876f2879024be7402fe3a4323c0bde3b8e2
Xun Li <xunli@asu.edu>
2015-11-17 15:19:23 -0700

    update version

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 namespace Gda {
 	const int version_major = 1;
 	const int version_minor = 7;
-	const int version_build = 63;
+	const int version_build = 67;
 	const int version_year = 2015;
-	const int version_month = 10;
-	const int version_day = 9;
+	const int version_month = 11;
+	const int version_day = 17;
 	const int version_night = 0;
 	const int version_type = 1; // 0: alpha, 1: beta, 2: release
 }
