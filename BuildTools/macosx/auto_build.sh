deb4d487d2893b223383febaf2ffa3fa677a3542
Xun Li <xunli@asu.edu>
2016-03-16 12:39:49 -0700

    update build with LOG script

--- BuildTools/macosx/auto_build.sh
+++ BuildTools/macosx/auto_build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 VERSION=$1
+NODEBUG=$2
 
 #launchctl load auto_build.plist
 cd ~/geoda_trunk/
 git checkout rc/GdaAppResources.cpp
 git pull
 cd ~/geoda_trunk/BuildTools/macosx
-./build.sh 16 1
+./build.sh 16 $DEBUG
 cd ~/Dropbox/yoursway-create-dmg
 geoda.sh $VERSION
 mv GeoDa$VERSION-Installer.dmg ~/Dropbox
