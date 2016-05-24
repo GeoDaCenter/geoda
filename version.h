ef9dc18f4bb5147a597fe664ddf0f7cd84ed262a
Xun Li <xunli@asu.edu>
2016-05-24 16:54:35 -0700

    update version to 1.8.7.7

--- version.h
+++ version.h
@@ -2,10 +2,10 @@ namespace Gda {
 	const int version_major = 1;
 	const int version_minor = 8;
 	const int version_build = 7;
-	const int version_subbuild = 5;
+	const int version_subbuild = 7;
 	const int version_year = 2016;
 	const int version_month = 5;
-	const int version_day = 22;
+	const int version_day = 24;
 	const int version_night = 0;
 	const int version_type = 1; // 0: alpha, 1: beta, 2: release
 }
