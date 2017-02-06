6378b3d455ae493bffa285bd408cf824a83ae264
Xun Li <xunli@asu.edu>
2017-02-06 15:27:41 -0600

    update toolbar size on windows

--- GeoDa.cpp
+++ GeoDa.cpp
@@ -328,7 +328,7 @@ bool GdaApp::OnInit(void)
 		frameHeight = 80;
 	}
 	if (GeneralWxUtils::isWindows()) {
-		frameWidth = 1080;
+		frameWidth = 1090;
 		frameHeight = 120;
 	}
 	if (GeneralWxUtils::isUnix()) {  // assumes GTK
