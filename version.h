3fee922949fe9342760ad846a1c0dcefb9b48b96
Xun Li <xunli@uchicago.edu>
2017-03-07 18:47:21 -0600

    fix issue 644

--- version.h
+++ version.h
@@ -2,7 +2,7 @@ namespace Gda {
 	const int version_major = 1;
 	const int version_minor = 8;
 	const int version_build = 16;
-	const int version_subbuild = 2;
+	const int version_subbuild = 4;
 	const int version_year = 2017;
 	const int version_month = 3;
 	const int version_day = 1;
