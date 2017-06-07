389d2f566b4427c9ede276a422cc0887fce2358b
Xun Li <xunli@uchicago.edu>
2017-06-07 16:40:28 -0500

    update version number 1.10.0.4 for tracking

--- version.h
+++ version.h
@@ -2,7 +2,7 @@ namespace Gda {
 	const int version_major = 1;
 	const int version_minor = 10;
 	const int version_build = 0;
-	const int version_subbuild = 2;
+	const int version_subbuild = 4;
 	const int version_year = 2017;
 	const int version_month = 6;
 	const int version_day = 5;
