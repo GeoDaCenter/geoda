32c60baaba223dafb5d46fec9184fe912eb42695
Xun Li <xunli@uchicago.edu>
2017-10-04 12:27:22 -0700

    update to testing version for auto-update release

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 namespace Gda {
 	const int version_major = 1;
 	const int version_minor = 12;
-	const int version_build = 0;
-	const int version_subbuild = 6;
+	const int version_build = 1;
+	const int version_subbuild = 1;
 	const int version_year = 2017;
 	const int version_month = 10;
 	const int version_day = 4;
