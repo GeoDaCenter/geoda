22dd3f6e197574e0cda98c9542057e96e50c7939
Xun Li <xunli@uchicago.edu>
2018-02-16 21:03:25 -0700

    update version for quick release 95

--- version.h
+++ version.h
@@ -5,7 +5,7 @@ namespace Gda {
 	const int version_subbuild = 95;
 	const int version_year = 2018;
 	const int version_month = 2;
-	const int version_day = 9;
+	const int version_day = 16;
 	const int version_night = 0;
 	const int version_type = 2; // 0: alpha, 1: beta, 2: release
 }
