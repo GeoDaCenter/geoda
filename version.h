a6c61751368c4e3fa11f261f68d9104d5d9bfeac
Xun Li <xunli@uchicago.edu>
2018-05-25 16:59:16 -0700

    update version

--- version.h
+++ version.h
@@ -2,10 +2,10 @@ namespace Gda {
 	const int version_major = 1;
 	const int version_minor = 12;
 	const int version_build = 1;
-	const int version_subbuild = 135;
+	const int version_subbuild = 137;
 	const int version_year = 2018;
 	const int version_month = 5;
-    const int version_day = 23;
+    const int version_day = 25;
 	const int version_night = 0;
 	const int version_type = 2; // 0: alpha, 1: beta, 2: release
 }
