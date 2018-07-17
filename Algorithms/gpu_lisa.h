21e7c215bbdd29e3314a51da2d6109f41ac1c0cf
Xun Li <xunli@uchicago.edu>
2018-07-17 12:07:50 -0700

    update: add local join count gpu code

--- Algorithms/gpu_lisa.h
+++ Algorithms/gpu_lisa.h
@@ -5,4 +5,7 @@
 
 bool gpu_lisa(const char* cl_path, int rows, int permutations, unsigned long long last_seed_used, double* values, double* local_moran, GalElement* w, double* p);
 
+
+bool gpu_localjoincount(const char* cl_path, int rows, int permutations, unsigned long long last_seed_used, int num_vars, int* values, int* local_jc, GalElement* w, double* p);
+
 #endif
