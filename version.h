dc6fdd51792b6d61e248c0840c6c4a6beabe9321
Xun Li <lixun910@gmail.com>
2018-12-17 16:52:33 -0700

    Update version.h

--- version.h
+++ version.h
@@ -6,6 +6,6 @@ namespace Gda {
 	const int version_year = 2018;
 	const int version_month = 12;
     const int version_day = 17;
-	const int version_night = 0;
+	const int version_night = 1;
 	const int version_type = 2; // 0: alpha, 1: beta, 2: release
 }
