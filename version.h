53e9b0f4d5fb204208fb302c9ba1a9ff28a71a6f
Xun Li <lixun910@gmail.com>
2019-04-18 22:07:05 -0700

    Update version.h

--- version.h
+++ version.h
@@ -5,7 +5,7 @@ namespace Gda {
     const int version_subbuild = 227;
 	const int version_year = 2019;
 	const int version_month = 4;
-    const int version_day = 16;
+    const int version_day = 18;
 	const int version_night = 0;
 	const int version_type = 2; // 0: alpha, 1: beta, 2: release
 }
