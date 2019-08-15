4c314636bcb36cda8146b395a5a12ba27644b287
lixun910 <lixun910@gmail.com>
2019-08-15 16:53:39 -0700

    update build scripts on Ubuntu xenial and packaging scripts

--- BuildTools/ubuntu/run_geoda.sh
+++ BuildTools/ubuntu/run_geoda.sh
@@ -6,7 +6,7 @@ GEODA_HOME=/usr/local/geoda
 export LD_LIBRARY_PATH=$GEODA_HOME/plugins:$ORACLE_HOME:$LD_LIBRARY_PATH
 export GDAL_DATA=$GEODA_HOME/gdaldata
 export OGR_DRIVER_PATH=$GEODA_HOME/plugins
-chmod +x $GEODA_HOME/plugins/*
-chmod +x $GEODA_HOME/cache.sqlite
-chmod +x $GEODA_HOME/GeoDa
+#chmod +x $GEODA_HOME/plugins/*
+#chmod +x $GEODA_HOME/cache.sqlite
+#chmod +x $GEODA_HOME/GeoDa
 exec "$GEODA_HOME/GeoDa"
