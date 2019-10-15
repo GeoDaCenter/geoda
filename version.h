547302ad25c7840aa219ac60637d6981058b18fe
Xun Li <xunli@uchicago.edu>
2019-10-15 15:09:48 -0700

    fix an issue of specifying CRS when saveas
    When there is no projection information of current dataset (e.g. no .prj file, or create points from a csv table), specifying CRS when exporting to a vector data format doesn't generate a .prj file or related projection information.

--- version.h
+++ version.h
@@ -5,7 +5,7 @@ namespace Gda {
     const int version_subbuild = 2;
 	const int version_year = 2019;
     const int version_month = 10;
-    const int version_day = 10;
+    const int version_day = 15;
 	const int version_night = 0;
 	const int version_type = 2; // 0: alpha, 1: beta, 2: release
 }
