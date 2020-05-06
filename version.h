54f1296a2501f06dcb2f2f8effab88c7d363c27e
Xun Li <lixun910@gmail.com>
2020-05-06 13:33:03 -0700

    Update version.h

--- version.h
+++ version.h
@@ -2,10 +2,10 @@ namespace Gda {
 	const int version_major = 1;
 	const int version_minor = 14;
 	const int version_build = 0;
-    const int version_subbuild = 46;
+    const int version_subbuild = 48;
 	const int version_year = 2020;
-    const int version_month = 4;
-    const int version_day = 27;
+    const int version_month = 5;
+    const int version_day = 4;
 	const int version_night = 0;
 	const int version_type = 2; // 0: alpha, 1: beta, 2: release
 }
