0ae6b55d06e3c9e08c512f585ca6bb26c45ad14d
Xun Li <xunli@uchicago.edu>
2020-11-20 18:01:58 -0700

    Update version.h

--- version.h
+++ version.h
@@ -2,7 +2,7 @@ namespace Gda {
 	const int version_major = 1;
 	const int version_minor = 16;
 	const int version_build = 0;
-    const int version_subbuild = 28;
+    const int version_subbuild = 30;
 	const int version_year = 2020;
     const int version_month = 11;
     const int version_day = 20;
