61d530860f0c8b743168eb64fb37489ef3e7008a
Xun Li <lixun910@gmail.com>
2023-06-05 20:25:07 -0700

    Update code_sign.py

--- BuildTools/macosx/code_sign.py
+++ BuildTools/macosx/code_sign.py
@@ -16,12 +16,24 @@ def ProcessDependency(dylib_path, cid):
         dylib_path = '/opt/homebrew/opt/geos/lib/libgeos.3.11.1.dylib'
     if dylib_path == '@rpath/libgeos.3.11.0.dylib':
         dylib_path = '/opt/homebrew/opt/geos/lib/libgeos.3.11.0.dylib'
+    if dylib_path == '@rpath/libgeos.3.11.2.dylib':
+        dylib_path = '/opt/homebrew/opt/geos/lib/libgeos.3.11.2.dylib'
     if dylib_path == '@loader_path/libicuuc.71.dylib':
         dylib_path = '/opt/homebrew/opt/icu4c/lib/libicuuc.71.dylib'
+    if dylib_path == '@loader_path/libicuuc.72.dylib':
+        dylib_path = '/opt/homebrew/opt/icu4c/lib/libicuuc.72.dylib'
     if dylib_path == '@loader_path/libicudata.71.dylib':
         dylib_path = '/opt/homebrew/opt/icu4c/lib/libicudata.71.dylib'
+    if dylib_path == '@loader_path/libicudata.72.dylib':
+        dylib_path = '/opt/homebrew/opt/icu4c/lib/libicudata.72.dylib'
     if dylib_path == '@loader_path/libbrotlicommon.1.dylib':
         dylib_path = '/opt/homebrew/opt/brotli/lib/libbrotlicommon.1.dylib'
+    if dylib_path == '@rpath/libsharpyuv.0.dylib':
+        dylib_path = '/opt/homebrew/opt/webp/lib/libsharpyuv.0.dylib'
+    if dylib_path == '@loader_path/libkmlbase.1.dylib':
+        dylib_path = '/opt/homebrew/opt/libkml/lib/libkmlbase.1.dylib'
+    if dylib_path == '@loader_path/libkmldom.1.dylib':
+        dylib_path = '/opt/homebrew/opt/libkml/lib/libkmldom.1.dylib'
 
     if dylib_path == '@rpath/libIlmThread-3_1.30.dylib':
         dylib_path = '/usr/local/opt/openexr/lib/libIlmThread-3_1.30.dylib'
