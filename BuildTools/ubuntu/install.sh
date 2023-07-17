403970b357f2a639a555dffb4181fa1b6c4ece59
Xun Li <lixun910@gmail.com>
2023-07-17 15:58:10 -0700

    Update install.sh

--- BuildTools/ubuntu/install.sh
+++ BuildTools/ubuntu/install.sh
@@ -10,6 +10,25 @@ cd BuildTools
 cd ubuntu
 export GEODA_HOME=$PWD 
 
+# Install libgdal
+export DEBIAN_FRONTEND=noninteractive
+$APT update -y
+# fix curl 60 error
+$APT install -y ca-certificates libgnutls30
+echo '-k' > ~/.curlrc
+$APT install -y libpq-dev
+$APT install -y gdal-bin
+$APT install -y libgdal-dev
+$APT install -y unzip cmake dh-autoreconf libgtk-3-dev libgl1-mesa-dev libglu1-mesa-dev 
+
+if  [ $OS = 'disco' ] ; then
+    $APT install -y libwebkit2gtk-4.0-dev
+elif  [ $OS = 'focal' ] ; then
+    $APT install -y libwebkit2gtk-4.0-dev
+else
+    $APT install -y libwebkitgtk-3.0-dev 
+fi
+
 # Build GeoDa
 cp ../../GeoDamake.$OS.opt ../../GeoDamake.opt
 make -j$(nproc)
