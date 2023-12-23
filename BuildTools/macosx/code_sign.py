be1ced4c263eb63c1b14b79571251841f6b510f6
Xun Li <lixun910@gmail.com>
2023-12-23 11:48:30 -0700

    libgdal.34

--- BuildTools/macosx/code_sign.py
+++ BuildTools/macosx/code_sign.py
@@ -109,4 +109,4 @@ def ProcessDependency(dylib_path, cid, current_item=None):
 # python3 code_sign.py /opt/homebrew/opt/gdal/lib/libgdal.29.dylib "Apple Development: xunli@uchicago.edu (AN5USPSZF6)"
 # python3 code_sign.py /opt/homebrew/opt/gdal/lib/libgdal.32.dylib "Apple Development: xunli@uchicago.edu (AN5USPSZF6)"
 # ProcessDependency(sys.argv[1], sys.argv[2])
-# ProcessDependency('/opt/homebrew/Cellar/gdal/3.7.2/lib/libgdal.33.3.7.2.dylib', "Apple Development: xunli@uchicago.edu (AN5USPSZF6)")
+# ProcessDependency('/opt/homebrew/Cellar/gdal/3.7.2/lib/libgdal.34.3.7.2.dylib', "Apple Development: xunli@uchicago.edu (AN5USPSZF6)")
