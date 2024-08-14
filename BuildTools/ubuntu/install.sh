f7cbd9c6c7aa8e14a20477386aa05115fed033ba
Xun Li <lixun910@gmail.com>
2024-08-14 16:02:12 -0700

    Update install.sh

--- BuildTools/ubuntu/install.sh
+++ BuildTools/ubuntu/install.sh
@@ -22,7 +22,11 @@ $APT install -y gdal-bin
 $APT install -y libgdal-dev
 $APT install -y unzip cmake dh-autoreconf libgtk-3-dev libgl1-mesa-dev libglu1-mesa-dev 
 
-if [ $OS = 'jammy' ] || [ $OS = 'focal' ] || [ $OS = 'noble' ]; then
+if  [ $OS = 'jammy' ] ; then
+    $APT install -y libwebkit2gtk-4.0-dev
+elif  [ $OS = 'focal' ] ; then
+    $APT install -y libwebkit2gtk-4.0-dev
+elif  [ $OS = 'noble' ] ; then
     $APT install -y libwebkit2gtk-4.0-dev
 else
     $APT install -y libwebkitgtk-3.0-dev 
