46d7d82f0d5e031b29278f3e1dc72bee636823f1
Xun Li <lixun910@gmail.com>
2024-08-14 20:05:14 -0700

    Update create_deps.sh

--- BuildTools/ubuntu/create_deps.sh
+++ BuildTools/ubuntu/create_deps.sh
@@ -30,9 +30,7 @@ $APT install -y gdal-bin
 $APT install -y libgdal-dev
 $APT install -y unzip cmake dh-autoreconf libgtk-3-dev libgl1-mesa-dev libglu1-mesa-dev 
 
-if  [ $OS = 'jammy' ] ; then
-    $APT install -y libwebkit2gtk-4.0-dev
-elif  [ $OS = 'focal' ] ; then
+if  [ $OS = 'jammy' ] || [ $OS = 'noble' ] || [ $OS = 'focal']; then
     $APT install -y libwebkit2gtk-4.0-dev
 else
     $APT install -y libwebkitgtk-3.0-dev 
