82a77be46e40cc6bc9a01e957e8dc36b7cf9b53f
Xun Li <lixun910@gmail.com>
2024-08-21 11:19:14 -0700

    test ubuntu 24.04 [ci skip]

--- BuildTools/ubuntu/install.sh
+++ BuildTools/ubuntu/install.sh
@@ -11,29 +11,8 @@ cd ubuntu
 export GEODA_HOME=$PWD 
 echo $GEODA_HOME
 
-# Install libgdal
-export DEBIAN_FRONTEND=noninteractive
-$APT update -y
-# fix curl 60 error
-$APT install -y ca-certificates libgnutls30
-echo '-k' > ~/.curlrc
-$APT install -y libpq-dev
-$APT install -y gdal-bin
-$APT install -y libgdal-dev
-$APT install -y unzip cmake dh-autoreconf libgtk-3-dev libgl1-mesa-dev libglu1-mesa-dev 
-
-if  [ $OS = 'jammy' ] ; then
-    $APT install -y libwebkit2gtk-4.0-dev
-elif  [ $OS = 'focal' ] ; then
-    $APT install -y libwebkit2gtk-4.0-dev
-elif  [ $OS = 'noble' ] ; then
-    $APT install -y libwebkit2gtk-4.1-dev
-else
-    $APT install -y libwebkitgtk-3.0-dev 
-fi
-
 # Build GeoDa
-cp ../../GeoDamake.$OS.opt ../../GeoDamake.opt
+cp ../../GeoDamake.ubuntu.opt ../../GeoDamake.opt
 make -j$(nproc)
 make app
 
